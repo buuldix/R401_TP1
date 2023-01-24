7f0845bc29309145681b25b0744552b5d4446719
bozonmat <bozonmat@D250-S03>
2023-01-24 14:41:48 +0100

    Code GetAll()

---
+++ WSConvertisseur/Controllers/DevisesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using WSConvertisseur.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WSConvertisseur.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevisesController : ControllerBase
+    {
+
+        List<Devise> devises = new() {new(1,"Dollar",1.08),new(2, "Franc Suisse",1.07),new(3,"Yen",120) };
+        public DevisesController()
+        {
+        }
+        // GET: api/<DevisesController>
+        [HttpGet]
+        public IEnumerable<Devise> GetAll()
+        {
+            return devises;
+        }
+
+        // GET api/<DevisesController>/5
+        [HttpGet("{id}")]
+        public string GetAll(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DevisesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DevisesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DevisesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
